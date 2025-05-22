19291cd3c21d224d2cdd1a3bbf78556fc7957f20
Alan Jahir Soto Miranda <alansoto@MacBook-Air-de-Alan.local>
2025-05-22 15:41:04 -0600

    ✅ Corrige rutas de imágenes para producción

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // <<--- ESTA ES LA CLAVE
+  base: '/', // MUY IMPORTANTE para producción
   plugins: [react()],
 })
